15f14b18bb06de670b5e3fae56f3b58e46875211
Yvan Duhamel <yvandev@outlook.fr>
2024-04-10 17:27:37 +0200

    4.2.0

--- variables.tf
+++ variables.tf
@@ -314,5 +314,5 @@ variable "vm_sp_storage_account_type" {
 }
 
 variable "_artifactsLocation" {
-  default = "https://raw.githubusercontent.com/Yvand/terraform-azurerm-sharepoint/4.1.0/dsc/"
+  default = "https://raw.githubusercontent.com/Yvand/terraform-azurerm-sharepoint/4.2.0/dsc/"
 }
