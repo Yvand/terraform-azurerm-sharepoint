8e8e0f633517a20b057a722694b14d37f31e1813
Yvand <yvandev@outlook.fr>
2024-11-15 09:27:18 +0100

    update Subscription-Latest

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ locals {
       "Label" : "Latest",
       "Packages" : [
         {
-          "DownloadUrl" : "https://download.microsoft.com/download/b/c/d/bcd4b3cb-25d1-4946-b8fd-b99ee62848bb/uber-subscription-kb5002649-fullfile-x64-glb.exe"
+          "DownloadUrl" : "https://download.microsoft.com/download/4/b/4/4b4d907f-2e25-4972-a7b1-dfe8d1c0fadb/uber-subscription-kb5002651-fullfile-x64-glb.exe"
         }
       ]
     }
