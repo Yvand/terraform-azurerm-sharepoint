dbd03a7b0d8b3a513de3b53299bbe82f2334831e
Yvand <yvandev@outlook.fr>
2025-07-09 15:22:09 +0200

    undo manual overrides

--- main.tf
+++ main.tf
@@ -327,7 +327,7 @@ module "vm_dc_def" {
   sku_size                   = var.vm_dc_size
   timezone                   = var.time_zone
   license_type               = local.license_type
-  zone                       = 1 #random_integer.zone_index.result
+  zone                       = random_integer.zone_index.result
   encryption_at_host_enabled = false
   patch_mode                 = "AutomaticByPlatform"
   secure_boot_enabled        = true
@@ -446,7 +446,7 @@ module "vm_sql_def" {
   sku_size                   = var.vm_sql_size
   timezone                   = var.time_zone
   license_type               = local.license_type
-  zone                       = 1 #random_integer.zone_index.result
+  zone                       = random_integer.zone_index.result
   encryption_at_host_enabled = false
   patch_mode                 = "AutomaticByOS"
   secure_boot_enabled        = true
@@ -564,7 +564,7 @@ module "vm_sp_def" {
   sku_size                   = var.vm_sp_size
   timezone                   = var.time_zone
   license_type               = local.license_type
-  zone                       = 1 #random_integer.zone_index.result
+  zone                       = random_integer.zone_index.result
   encryption_at_host_enabled = false
   patch_mode                 = local.is_sharepoint_subscription ? "AutomaticByPlatform" : "AutomaticByOS"
   secure_boot_enabled        = local.vms_settings.vms_sharepoint_trustedLaunchEnabled
@@ -715,7 +715,7 @@ module "vm_fe_def" {
   sku_size                   = var.vm_sp_size
   timezone                   = var.time_zone
   license_type               = local.license_type
-  zone                       = 1 #random_integer.zone_index.result
+  zone                       = random_integer.zone_index.result
   encryption_at_host_enabled = false
   patch_mode                 = local.is_sharepoint_subscription ? "AutomaticByPlatform" : "AutomaticByOS"
   secure_boot_enabled        = local.vms_settings.vms_sharepoint_trustedLaunchEnabled
