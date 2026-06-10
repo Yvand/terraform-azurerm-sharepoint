1cebb2fee8bec8c62344ded12f984367a9da33e2
Yvand <yvandev@outlook.fr>
2026-06-10 16:26:01 +0200

    work

--- variables.tf
+++ variables.tf
@@ -427,7 +427,7 @@ variable "vm_sp_storage" {
 variable "_artifactsLocation" {
   type        = string
   description = "The base URI where artifacts required by this template are located including a trailing '/'"
-  default     = "https://github.com/Yvand/SharePointInfraDsc/releases/download/releases/v3.1.0/"
+  default     = "https://github.com/Yvand/SharePointInfraDsc/releases/download/releases/v3.1.1/"
 }
 
 variable "tags" {
